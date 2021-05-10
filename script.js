fef6a9b09cb1fe4a174e3186711d36da5a90fd9e
tod619 <trevorodwyer619@hotmail.com>
2021-05-10 16:34:32 +0100

    add functionality to banner btn in script.js file

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
 
+    // Navbar btn funtcionality
+    $('.btn').click(function(){
+        $('.nav-links').slideToggle(1000)
+        $('.btn').toggleClass('change')
+    })
 })
