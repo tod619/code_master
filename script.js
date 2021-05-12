502932d8388ad478f2334f202a37b4202879f122
tod619 <trevorodwyer619@hotmail.com>
2021-05-12 14:32:01 +0100

    add bg-color to nav bg on scroll in script.js file

--- script.js
+++ script.js
@@ -5,4 +5,15 @@ $(document).ready(function(){
         $('.nav-links').slideToggle(1000)
         $('.btn').toggleClass('change')
     })
+
+    // Transparent navbar background on scroll
+    $(window).scroll(function(){
+        let position = $(window).scrollTop()
+
+        if(position >= 100) {
+            $('nav, .nav-container').addClass('nav-background')
+        } else {
+            $('nav, .nav-container').removeClass('nav-background')
+        }
+    })
 })
